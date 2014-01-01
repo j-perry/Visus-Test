f2f9d8d1b0addfd8f00002339ed126f3e3f691a3
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-01-01 11:40:03 +0000

    Test code base
    Removed TestUser.class. Have added a new package, including a new class,
    'TestSortActivities'.

--- src/com/visus/test/AllTests.java
+++ src/com/visus/test/AllTests.java
@@ -11,7 +11,6 @@ import android.test.AndroidTestCase;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	TestUser.class, 
 	TestSession.class,
 	TestSessionHandler.class
 })
---
+++ src/com/visus/test/settings/activities/TestSortActivities.java
@@ -0,0 +1,31 @@
+package com.visus.test.settings.activities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import android.test.AndroidTestCase;
+
+public class TestSortActivities extends AndroidTestCase {
+
+	public TestSortActivities() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
