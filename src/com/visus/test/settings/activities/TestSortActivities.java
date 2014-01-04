e8ca82caa46f4d9a4321f8565aaae8759e2393f4
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-01-04 22:09:28 +0000

    Code base
    Implemented a selection sort algorithm in project. Test case now
    successfully works.
    
    Will need to refactor sorting code; currently sorts from lowest -
    highest. Once list is returned, it is reversed using an instance of the
    Collections object. It isn't a particularly elegant and efficient
    solution.

--- src/com/visus/test/settings/activities/TestSortActivities.java
+++ src/com/visus/test/settings/activities/TestSortActivities.java
@@ -3,6 +3,9 @@ package com.visus.test.settings.activities;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 import org.junit.After;
@@ -12,6 +15,7 @@ import org.junit.Test;
 import com.visus.settings.activities.ActivitiesManager;
 
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 public class TestSortActivities extends AndroidTestCase {
 
@@ -34,27 +38,27 @@ public class TestSortActivities extends AndroidTestCase {
 		// assume we have retrieved each activity type and the durations 
 		// for the sessions associated with each one from the db
 		// activity 'n'
-		Stack<double []> activities = new Stack<double []>();
+		Stack<Double []> activities = new Stack<Double []>();
 		ActivitiesManager am = new ActivitiesManager();
-		double [] activity1 = { 24.59, 24.59, 14.31, 7.11 };
-		double [] activity2 = { 24.20, 21.31, 14.31, 7.11, 13.41 };
-		double [] activity3 = { 10.40, 24.59, 3.51, 7.03 };
-		double [] activity4 = { 2.39, 12.19, 18.12, 22.13 };
+		Double [] activity1 = { 24.59, 24.59, 14.31, 7.11 };
+		Double [] activity2 = { 24.20, 21.31, 14.31, 7.11, 13.41 };
+		Double [] activity3 = { 10.40, 24.59, 3.51, 7.03 };
+		Double [] activity4 = { 2.39, 12.19, 18.12, 22.13 };
 		
 		// test results
-		double testResult1 = 70.60;
-		double testResult2 = 80.34;
-		double testResult3 = 45.53;
-		double testResult4 = 54.83;
+		Double testResult1 = 70.60;
+		Double testResult2 = 80.34;
+		Double testResult3 = 45.53;
+		Double testResult4 = 54.83;
 		String testResult = new String(String.valueOf(testResult2) + ", " +
 								   	   String.valueOf(testResult1) + ", " +
 								       String.valueOf(testResult4) + ", " +
 								       String.valueOf(testResult3) );
-		String result = null;
+		StringBuilder result = null;
 		
 		@SuppressWarnings("unused")
-		double [] unsortedActivities = null,
-				  sortedActivities= null;
+		Double [] unsortedActivities = new Double[4],
+				  sortedActivities = new Double[4];
 		int i = 0;
 		
 		activities.push(activity1);
@@ -64,20 +68,30 @@ public class TestSortActivities extends AndroidTestCase {
 		
 		// ok; we now need to add each of the values for each activity
 		// run a while loop that takes input of each set of integers
-		// returning the value of each set	
+		// returning the value of each set
 		while(activities.size() != 0) {
-			double [] activity = activities.pop();
+			Double [] activity = activities.pop();
 			unsortedActivities[i++] = am.add(activity);
 		}
 		
 		sortedActivities = am.sort(unsortedActivities);
 		
-		result = new String(String.valueOf(sortedActivities[0]) + ", " +
-							String.valueOf(sortedActivities[1]) + ", " +
-							String.valueOf(sortedActivities[2]) + ", " +
-							String.valueOf(sortedActivities[3]) );
+		ArrayList<Double> newResult = new ArrayList<Double>(Arrays.asList(sortedActivities) );
+		Collections.reverse(newResult);
+		result = new StringBuilder();
+		
+		for(i = 0; i < newResult.size(); i++) {
+			if(i == newResult.size() -1) {
+				result.append( String.valueOf(newResult.get(i) ));
+			}
+			else {
+				result.append( String.valueOf(newResult.get(i) ) + ", ");
+			}
+		}
 		
 		// check whether sorted results match with sorted results defined
-		assertEquals(result, testResult);
+		assertEquals(testResult, result.toString());
+		Log.e("Visus", "testResult: " + testResult);
+		Log.e("Visus", "result: " + result.toString());
 	}
 }
